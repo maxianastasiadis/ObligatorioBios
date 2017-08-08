84c13183df0ec21348206e33e78149bb7a809a45
nicolas <>
2017-08-08 00:19:44 -0300

    Mapa centros

--- MiBiosWeb/src/java/com/mibios/web/managedbeans/CursosBean.java
+++ MiBiosWeb/src/java/com/mibios/web/managedbeans/CursosBean.java
@@ -15,6 +15,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
 
 /**
  *
@@ -26,6 +30,7 @@ public class CursosBean {
 
     private List<CursosDatos> listaCursos;
     private List<ClaseDatos> listaClases;
+    private MapModel modeloParaMapaCentrosBios;
     
     public CursosBean() {
         try {
@@ -44,6 +49,22 @@ public class CursosBean {
                 listaCursos.add(cursoDatos);
             }
             
+            modeloParaMapaCentrosBios = new DefaultMapModel();
+          
+            //Puntos
+            LatLng centroTecnicoLaboral = new LatLng(-34.9014832, -56.1384523);
+            LatLng centroSistemasTecnologias = new LatLng(-34.905768,-56.1911781);
+            LatLng centroNegociosAdministracion = new LatLng(-34.9051878,-56.1969757);
+            LatLng centroDisenioComunicacion = new LatLng(-34.9117959,-56.1634542);
+            LatLng centroEconomiaFinanzas = new LatLng(-34.9022061,-56.1761806);
+
+            //Marcadores
+            modeloParaMapaCentrosBios.addOverlay(new Marker(centroTecnicoLaboral, "Formación Técnico Laboral"));
+            modeloParaMapaCentrosBios.addOverlay(new Marker(centroSistemasTecnologias, "Sistemas y Tecnologías"));
+            modeloParaMapaCentrosBios.addOverlay(new Marker(centroNegociosAdministracion, "Negocios y Administración"));
+            modeloParaMapaCentrosBios.addOverlay(new Marker(centroDisenioComunicacion, "Diseño y Comunicación"));
+            modeloParaMapaCentrosBios.addOverlay(new Marker(centroEconomiaFinanzas, "Economía y Finanzas"));
+            
         } catch (Exception ex) {
             Logger.getLogger(CuentaCorrienteBean.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -60,5 +81,13 @@ public class CursosBean {
     public void setListaClases(List<ClaseDatos> listaClases) {
         this.listaClases = listaClases;
     }
+
+    public MapModel getModeloParaMapaCentrosBios() {
+        return modeloParaMapaCentrosBios;
+    }
+
+  
+    
+    
     
 }
